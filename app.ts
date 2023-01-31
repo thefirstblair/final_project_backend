a55f81febd5833df986b12f41e292e88594598a2
thanyatorn <60596658+thefirstblair@users.noreply.github.com>
2023-01-31 23:30:43 +0700

    feat: add chat room feature with socket io

--- app.ts
+++ app.ts
@@ -8,8 +8,10 @@ import noteRouter from "./routers/noteRouter";
 import featureRouter from "./routers/featureRouter";
 import cookieSession from "cookie-session";
 import cors from "cors";
+import { Server } from "socket.io";
 
 const prisma = new PrismaClient();
+const io = new Server();
 const app = express();
 const port = 3001;
 
@@ -39,9 +41,58 @@ app.use("/rooms", roomRouter);
 app.use("/notes", noteRouter);
 app.use("/features", featureRouter);
 
+const rooms: Record<string, any> = {};
+
+io.on("connection", (socket) => {
+  socket.on("set name", (name) => {
+    socket.data.name = name;
+    socket.emit("server_response", "You name has been set.");
+  });
+
+  socket.on("create room", (room_name) => {
+    if (!socket.data.name) return;
+    if (rooms[room_name]) return;
+
+    rooms[room_name] = {
+      name: room_name,
+      messages: [],
+      users: [socket.data.name],
+      background: 'default',
+    };
+
+    socket.data.room_name = room_name;
+    socket.join(room_name);
+    io.to(socket.data.room_name).emit("room_update", rooms[socket.data.room_name]);
+    socket.emit("server_response", "You joined " + room_name);
+    socket.emit("server_response", "Room has been created.");
+  });
+
+  socket.on("join room", (room_name) => {
+    if (!socket.data.name) return;
+    if (!rooms[room_name]) return;
+
+    socket.data.room_name = room_name;
+    socket.join(room_name);
+
+    rooms[room_name].users.push(socket.data.name);
+
+    io.to(socket.data.room_name).emit("room_update", rooms[socket.data.room_name]);
+    socket.emit("server_response", "You joined " + room_name);
+  });
+
+  socket.on("message", (message) => {
+    if (!socket.data.room_name) return;
+
+    rooms[socket.data.room_name].messages.push(socket.data.name,' : ',message);
+
+    io.to(socket.data.room_name).emit("room_update", rooms[socket.data.room_name]);
+  });
+});
+
 app.listen(port, async () => {
   await userSeeder();
   //await roomSeeder()
+  io.listen(3002);
   console.log("Server is running on http://localhost:" + port);
 });
 
