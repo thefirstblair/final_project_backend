9644142a14f3de20771483dfb594289e6597872a
thanyatorn <60596658+thefirstblair@users.noreply.github.com>
2023-02-05 02:56:23 +0700

    Edit Horoscope Text

--- routers/featureRouter.ts
+++ routers/featureRouter.ts
@@ -4,7 +4,12 @@ import express from "express";
 const featureRouter = express.Router()
 const prisma = new PrismaClient();
 
-const words = ['Horoscope 1', 'Horoscope 2', 'Horoscope 3', 'Horoscope 4', 'Horoscope 5'];
+const words = ['A light heart carries you through all the hard times.', 
+'A lifetime of happiness lies ahead of you.', 
+'The smart thing to do is to begin trusting your intuitions.', 
+'Strong reasons make strong actions. ', 
+"Don't let your limitations overshadow your talents"
+, 'Practice makes perfect.', 'Your talents will be recognized and suitably rewarded.'];
 
 function generateRandomWord() {
     const randomIndex = Math.floor(Math.random() * words.length);
