8f1c8a16b00e5693d1f563af5866ffc7a1161f4b
Pond26127 <pond261272@gmail.com>
2023-03-21 04:02:21 +0700

    Update app.ts

--- app.ts
+++ app.ts
@@ -603,7 +603,7 @@ io.on('connection' , (socket)=> {
     });
 
     socket.data.survey = createdSurvey;
-    io.to(socket.data.user.roomId).emit('resultCreateSurvey', createdSurvey);
+    io.to(socket.data.user.roomId).emit('resultCreateSurvey', socket.data.survey);
     console.log(socket.data.survey);
   });
 
